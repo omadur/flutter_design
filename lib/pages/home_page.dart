40f977acfea5b6744059b22b81432c2dd831b4f7
omadur <omarduran1989@hotmail.com>
2021-01-26 06:38:24 -0500

    Creacion del circle container

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_design/widgets/circle_container.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: CircleContainer(
+          child: Text(
+            'WELCOME',
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
+      ),
+    );
   }
 }
